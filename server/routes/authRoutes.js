4b8e0932e0be09d25d40161567c5fe8f551448d0
UdulaPahasara <udulapahasara2@gmail.com>
2026-07-02 01:28:59 -0700

    remove the rate limit dependency and feature

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import rateLimit from 'express-rate-limit';
 import { loginUser, forgotPassword, resetPassword } from '../controllers/authController.js';
 
 const router = express.Router();
 
 // POST /api/auth/login
-const loginLimiter = rateLimit({ 
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3,                   // Only 3 tries allowed
-  message: "Too many login attempts, please try again after 15 minutes" 
-});
-router.post('/login', loginLimiter, loginUser);
+router.post('/login', loginUser);
 
 // POST /api/auth/forgot-password
 router.post('/forgot-password', forgotPassword);
